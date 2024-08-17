a611d9d4774e3a66904f3393656f539b88beafe1
jhoan28310576 <jhoan19634@gmail.com>
2024-08-17 03:52:38 -0400

    v3
    modificaciones en todo la pagina  hay que trabajar en el apartdo de  works-section tengo que hacer 4 app minimo

--- js/main.js
+++ js/main.js
@@ -240,45 +240,20 @@ Description: Gerold - Personal Portfolio HTML5 Template
 		});
 		wow.init();
 
+
 		/*------------------------------------------------------
-  	/  Preloader
+  	/  conService
   	/------------------------------------------------------*/
-		const svg = document.getElementById("preloaderSvg");
-		const svgText = document.querySelector(
-			".hero-section .intro_text svg text"
-		);
-		const tl = gsap.timeline({
-			onComplete: startStrokeAnimation,
+		$(document).ready(function() {
+			$('select.tj-nice-select').niceSelect();
 		});
-		const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-		const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+		/*------------------------------------------------------
+  	/  fin conService
+  	/------------------------------------------------------*/
 
-		tl.to(".preloader-heading .load-text , .preloader-heading .cont", {
-			delay: 1.5,
-			y: -100,
-			opacity: 0,
-		});
-		tl.to(svg, {
-			duration: 0.5,
-			attr: { d: curve },
-			ease: "power2.easeIn",
-		}).to(svg, {
-			duration: 0.5,
-			attr: { d: flat },
-			ease: "power2.easeOut",
-		});
-		tl.to(".preloader", {
-			y: -1500,
-		});
-		tl.to(".preloader", {
-			zIndex: -1,
-			display: "none",
-		});
 
-		function startStrokeAnimation() {
-			// Add a class or directly apply styles to trigger the stroke animation
-			svgText.classList.add("animate-stroke");
-		}
+		/*------------------------------------------------------
+
 
 		/*------------------------------------------------------
   	/  Services Hover BG
